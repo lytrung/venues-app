609101a4aa61225c7ff0dc6c9f23fa37d53f18e8
Trung Ly <trung.ly@cit3-501m-01.acg.ac.nz>
2019-08-09 12:30:55 +1200

    Template

--- src/App.js
+++ src/App.js
@@ -1,26 +1,113 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  constructor(props){
+    super(props)
+
+    this.state = {
+      venues:[
+        {
+          id: "4bc992e7b6c49c7401a28e91", 
+          name: "Ken Yakitori", 
+          address: Array(3), 
+          category: "Japanese"
+        },
+        {
+          id: "4b4d4133f964a52070cf26e3", 
+          name: "Real Groovy", 
+          address: Array(3), 
+          category: "Record Shop"
+        },
+        {
+          id: "4b53a56bf964a52013a627e3", 
+          name: "Revel! Cafe", 
+          address: Array(3), 
+          category: "Café"
+        }
+      ]
+    }
+  }
+  render(){
+    return (
+      <div className="app">
+        <div className="container">
+          <div className="venues">
+
+            <div className="card venue">
+              <div className="card-body">
+                <h1 className="venue-name" data-toggle="modal" data-target="#venue-modal">The Store</h1>
+                <p>5B Gore St</p>
+                <p>Auckland</p>
+                <p><span className="badge venue-type">Café</span></p>
+              </div>
+            </div>
+            <div className="card venue">
+              <div className="card-body">
+                <h1 className="venue-name">The Store</h1>
+                <p>5B Gore St</p>
+                <p>Auckland</p>
+                <p><span className="badge venue-type">Café</span></p>
+              </div>
+            </div>
+            <div className="card venue">
+              <div className="card-body">
+                <h1 className="venue-name">The Store</h1>
+                <p>5B Gore St</p>
+                <p>Auckland</p>
+                <p><span className="badge venue-type">Café</span></p>
+              </div>
+            </div> 
+          </div> 
+
+          <div className="venue-filters">
+            
+            <div className="btn-group btn-group-toggle" data-toggle="buttons">
+              <div role="group" className="btn-group btn-group-toggle">
+                <label className="venue-filter btn active btn-primary">
+                  <input name="venue-filter" type="radio" autoComplete="off" value="all"/>All
+                </label>
+                <label className="venue-filter btn btn-primary">
+                  <input name="venue-filter" type="radio" autoComplete="off" value="food"/>Food
+                </label>
+                <label className="venue-filter btn btn-primary">
+                  <input name="venue-filter" type="radio" autoComplete="off" value="drinks"/>Drinks
+                </label>
+                <label className="venue-filter btn btn-primary">
+                  <input name="venue-filter" type="radio" autoComplete="off" value="others"/>Others
+                </label>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="modal" id="venue-modal" tabIndex="-1" role="dialog">
+          <div className="modal-dialog" role="document">
+            <div className="modal-content">
+       
+              <div className="modal-body">
+
+                <div className="venue-popup-body row">
+                  <div className="col-6">
+                    <h1 className="venue-name">The Store</h1>
+                    <p>5B Gore St</p>
+                    <p>Auckland</p>
+                    <p><span className="badge venue-type">Café</span></p>
+                  </div>
+                  <div className="col-6">
+                    <img src="https://fastly.4sqi.net/img/general/200x200/194220_nI7vTqtIFQncbe7Zgn_XLymzqM78Cx-aZ_gySunjz-M.jpg" className="img-fluid" alt="Responsive image"/>
+                  </div>
+                </div>
+
+              </div>
+
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
